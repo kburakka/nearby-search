06dc6a0a2e3f00d33367967c8ff8d44c6732a209
Burak <burak95kaya@hotmail.com>
2022-01-08 17:00:33 +0100

    application folder structure is organized

---
+++ nearby/nearby/App/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  nearby
+//
+//  Created by Burak Kaya on 08.01.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let bounds = UIScreen.main.bounds
+        window = UIWindow(frame: bounds)
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
